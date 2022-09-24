6fa02c8e5bbe9a9a6f6fe5c4ffa077774aae2e69
Van Hoang <vanhoang151101@gmail.com>
2022-09-24 20:14:57 +0700

    OOP - Quản lý sinh viên sửa Sắp xếp

--- baitap3/opp.rb
+++ baitap3/opp.rb
@@ -29,21 +29,21 @@ class Student < Man
         puts "Tên : #{name}, Giới tính: #{gender},  Tuổi: #{age}, Tóc: #{hair}, Chiều cao: #{height}(cm), Cân nặng: #{height}(kg), Số điện thoại: #{phone}, Email: #{email}, Dân tộc: #{nation}, Điểm trung bình: #{average}\n"
     end
 end
-a = Array.new()
+arr = Array.new()
 Student1 = Student.new("Hoang","Nam","Trắng","1m6","45kg","21","0123456789","hoangg@gmail.com","Kinh")
 Student1.diem(9,9,9)
-a.push(Student1)
+arr.push(Student1)
 Student2 = Student.new("Tuan","Nam","Vàng","1m9","45kg","28","0123456789","aaa@gmail.com","Kinh")
 Student2.diem(7,3,5)
-a.push(Student2)
+arr.push(Student2)
 Student3 = Student.new("Huy","Nam","Đen","1m7","45kg","23","0123456789","bbb@gmail.com","Kinh")
 Student3.diem(4,9,9)
-a.push(Student3)
-a.each_index {
+arr.push(Student3)
+arr.each_index {
     |index|
-    puts "#{a[index].show()}"
+    puts "#{arr[index].show()}"
 }
 print "Danh sách sinh viên sau khi sắp xếp:\n"
-for x in a.sort_by{|stu|[stu.average]} do
+for x in arr.sort_by{|stu| [stu.average]}.reverse do
     puts "#{x.show}"
 end
